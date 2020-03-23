f6e5c60c06af8f065ceae7031f2978fc4c5f9497
Tito Ciuro <tito@webbo.com>
2020-03-22 18:05:54 -0700

    Cleanup

--- MultipleCoordinators/Application/SceneDelegate.swift
+++ MultipleCoordinators/Application/SceneDelegate.swift
@@ -19,15 +19,9 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-//    var coordinator: MainCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-//        let navController = UINavigationController()
-//        navController.title = "Hello"
-//        coordinator = MainCoordinator(navigationController: navController)
-//        coordinator?.start()
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
@@ -47,4 +41,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {}
     func sceneDidEnterBackground(_ scene: UIScene) {}
 }
-
